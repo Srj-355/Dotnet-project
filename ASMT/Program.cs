2ca5255d0ff0890d868ee42c1d54f5feec0a0053
srj-355 <saroj80977@gmail.com>
2024-09-10 10:46:05 +0545

    Suman commit

--- ASMT/Program.cs
+++ ASMT/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//  hello saroj vai Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
